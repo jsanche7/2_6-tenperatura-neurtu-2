2d57b5a2ce7f972968a346e614dac2addc7bcd30
jsanche7 <80990132+jsanche7@users.noreply.github.com>
2021-10-27 23:40:16 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,6 @@
 let Tenperatura = 0
 music.setVolume(37)
 basic.forever(function () {
-    Tenperatura = input.temperature()
-    basic.showNumber(input.temperature())
-    if (Tenperatura < 30) {
-        basic.showIcon(IconNames.Chessboard)
-    }
-    if (Tenperatura > 30) {
-        basic.showIcon(IconNames.Square)
-    }
-})
-basic.forever(function () {
     if (Tenperatura < 30) {
         music.playTone(392, music.beat(BeatFraction.Quarter))
         music.playTone(330, music.beat(BeatFraction.Quarter))
@@ -18,5 +8,15 @@ basic.forever(function () {
     if (Tenperatura > 30) {
         music.playTone(740, music.beat(BeatFraction.Quarter))
         music.playTone(622, music.beat(BeatFraction.Quarter))
+    }
+})
+basic.forever(function () {
+    Tenperatura = input.temperature()
+    basic.showNumber(input.temperature())
+    if (Tenperatura < 30) {
+        basic.showIcon(IconNames.Chessboard)
+    }
+    if (Tenperatura > 30) {
+        basic.showIcon(IconNames.Square)
     }
 })
